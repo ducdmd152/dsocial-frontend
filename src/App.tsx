5836535ebfa57f742f10056c11d36ba5cd677bf6
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-04-06 08:32:09 +0700

    Set up layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Grid
+      templateAreas={{
+        base: `"main aside-right"`,
+        lg: `"aside-left main aside-right"`,
+      }}
+      templateColumns={{
+        base: `1fr 140px`,
+        sm: `1fr 200px`,
+        lg: "240px 1fr 240px",
+      }}
+      h="100%"
+    >
+      <Show above="lg">
+        <GridItem area="aside-left">Aside left</GridItem>
+      </Show>
+
+      <GridItem bg="#24273b" area="main">
+        Main
+      </GridItem>
+      <GridItem area="aside-right">Aside right</GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
