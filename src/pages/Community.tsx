5eb1d267c4279f2c76913f14fc90b7c1f1870fb0
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-04-07 10:39:53 +0700

    Setup router for auth & community

---
+++ src/pages/Community.tsx
@@ -0,0 +1,41 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import AsideLeft from "../components/AsideLeft";
+import AsideRight from "../components/AsideRight";
+import PostArea from "../components/PostArea";
+import useGetUser from "../hooks/useGetUser";
+
+function Community() {
+  const { user, error } = useGetUser(2);
+
+  if (error) return null;
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"main aside-right"`,
+        lg: `"aside-left main aside-right"`,
+      }}
+      templateColumns={{
+        base: `1fr 140px`,
+        sm: `1fr 200px`,
+        lg: "240px 1fr 240px",
+      }}
+      h="100%"
+    >
+      <Show above="lg">
+        <GridItem area="aside-left" className="aside-left">
+          <AsideLeft />
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" className="main">
+        <PostArea user={user} />
+      </GridItem>
+      <GridItem area="aside-right" className="aside-right">
+        <AsideRight user={user} />
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default Community;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+const Login = () => {
+  //   let redirect = true;
+  //   if (redirect) {
+  //     useEffect(() => {
+  //       setTimeout(() => window.location.replace("/commnunity"), 500);
+  //     });
+  //   }
+
+  return <div>Login</div>;
+};
+
+export default Login;
