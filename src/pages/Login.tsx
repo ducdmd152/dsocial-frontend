5a08f875514955bc694b6ea9af0cfde9ee9af0fa
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-04-07 11:42:41 +0700

    Completed login feature

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,69 +1,89 @@
 import { Button } from "@chakra-ui/button";
 import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
-import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
 import { Flex, HStack, Heading, Text } from "@chakra-ui/layout";
-
-import { Switch } from "@chakra-ui/switch";
-import React, { useEffect } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { useEffect } from "react";
+import authService from "../services/auth-service";
 
 const Login = () => {
-  let authenticated = false;
+  let authenticated = sessionStorage.getItem("user");
   if (authenticated) {
     useEffect(() => {
       setTimeout(() => window.location.replace("/community"), 500);
     });
   }
 
+  const { register, handleSubmit } = useForm();
+
   const { toggleColorMode } = useColorMode();
   const formBackground = useColorModeValue("gray.100", "gray.700");
+
+  const onSubmit = (data: FieldValues) => {
+    const checkLogin = async () => {
+      let res = await authService.login(data.username, data.password);
+      if (res) {
+        window.location.replace("/community");
+      }
+    };
+    checkLogin();
+  };
+
   return (
     <Flex h="100vh" alignItems="center" justifyContent="center">
-      <Flex
-        flexDirection="column"
-        bg={formBackground}
-        p={12}
-        borderRadius={8}
-        boxShadow="lg"
-        width="400px"
-        maxWidth="100%"
-      >
-        <Heading mb={6}>Login</Heading>
-        <Input placeholder="Username" type="text" variant="filled" mb={3} />
-        <Input
-          placeholder="**********"
-          type="password"
-          variant="filled"
-          mb={6}
-        />
-        <Button colorScheme="teal" mb={8}>
-          Login
-        </Button>
-
-        <HStack color="gray.400" marginX="auto">
-          <Text>Create a new account?</Text>
-          <Button
-            color="gray.500"
-            fontWeight="500"
-            variant={"link"}
-            onClick={() => window.location.replace("/register")}
-          >
-            Register
-          </Button>
-        </HStack>
-
-        <Button
-          mt="3"
-          color="gray.400"
-          fontWeight="300"
-          fontStyle="italic"
-          variant={"link"}
-          onClick={() => window.location.replace("/community")}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Flex
+          flexDirection="column"
+          bg={formBackground}
+          p={12}
+          borderRadius={8}
+          boxShadow="lg"
+          width="400px"
+          maxWidth="100%"
         >
-          ---Continue without login---
-        </Button>
+          <Heading mb={6}>Login</Heading>
+          <Input
+            placeholder="Username"
+            {...register("username")}
+            type="text"
+            variant="filled"
+            mb={3}
+          />
+          <Input
+            {...register("password")}
+            placeholder="**********"
+            type="password"
+            variant="filled"
+            mb={6}
+          />
+          <Button colorScheme="teal" mb={8} type="submit">
+            Login
+          </Button>
 
-        {/* <FormControl display="flex" alignItems="center">
+          <HStack color="gray.400" marginX="auto">
+            <Text>Create a new account?</Text>
+            <Button
+              color="gray.500"
+              fontWeight="500"
+              variant={"link"}
+              onClick={() => window.location.replace("/register")}
+            >
+              Register
+            </Button>
+          </HStack>
+
+          <Button
+            mt="3"
+            color="gray.400"
+            fontWeight="300"
+            fontStyle="italic"
+            variant={"link"}
+            onClick={() => window.location.replace("/community")}
+          >
+            ---Continue without login---
+          </Button>
+
+          {/* <FormControl display="flex" alignItems="center">
           <FormLabel htmlFor="dark_mode" mb="0">
             Dark Mode?
           </FormLabel>
@@ -74,7 +94,8 @@ const Login = () => {
             onChange={toggleColorMode}
           />
         </FormControl> */}
-      </Flex>
+        </Flex>
+      </form>
     </Flex>
   );
 };
